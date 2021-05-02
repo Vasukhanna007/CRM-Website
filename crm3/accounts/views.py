f0230316f4fa7d3874d4126e2e8c73a39858df48
Vasu Khanna <vasu.khanna@mail.mcgill.ca>
2021-05-03 02:50:44 +0530

    filteradded

--- crm3/accounts/views.py
+++ crm3/accounts/views.py
@@ -1,8 +1,10 @@
 from accounts.models import Product
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from .models import *
 from .forms import OrderForm
+from .filters import OrderFilter
 # Create your views here.
 
 
@@ -30,20 +32,30 @@ def customer(request, pk_test):
     # way of grabbing customer child object
     orders = customer.order_set.all()
     order_count = orders.count()
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders = myFilter.qs
+
     context = {'customer': customer,
-               'orders': orders, 'order_count': order_count}
+               'orders': orders, 'order_count': order_count, 'myFilter': myFilter}
     return render(request, 'accounts/customers.html', context)
 
 
-def createOrder(request):
-    form = OrderForm()
-    form = OrderForm(request.POST)
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(
+        Customer, Order, fields=('product', 'status'), extra=10)
+
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)
+    # form = OrderForm(initial={'customer': customer})
     if request.method == 'POST':
-        if form.is_valid():
+        # form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
+
+        if formset.is_valid():
             # model from will save infoo to db
-            form.save()
+            formset.save()
             return redirect('/')
-    context = {'form': form}
+    context = {'formset': formset}
     return render(request, 'accounts/order_form.html', context)
 
 
